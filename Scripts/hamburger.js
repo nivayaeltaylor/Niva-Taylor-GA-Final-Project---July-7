841c11ffc092b33732091be92852fda6bf221fee
nivayaeltaylor <nivayael@gmail.com>
2021-07-07 20:59:08 -0700

    Initial commit

---
+++ Scripts/hamburger.js
@@ -0,0 +1,22 @@
+(function() {
+
+	let hamburger = {
+		nav: document.querySelector('#nav'),
+		navToggle: document.querySelector('.nav-toggle'),
+
+		initialize() {
+			this.navToggle.addEventListener('click', () => { this.toggle(); });
+		},
+
+		toggle() {
+			this.navToggle.classList.toggle('expanded');
+			this.nav.classList.toggle('expanded');
+		},
+	};
+
+	hamburger.initialize();
+
+}());
+
+
+  console.log("hi there");
